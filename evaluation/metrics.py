482abe4a5c0e6c6b3d270d48822be432adeb1998
alessandro <alelombo55@gmail.com>
2020-12-14 01:19:50 +0100

    Add dice loss.

--- evaluation/metrics.py
+++ evaluation/metrics.py
@@ -1,6 +1,8 @@
 import torch
 
-IoU_smoothing_coeff = 1e-6
+
+# smoothing value to avoid undefined and undetermined operations during the computations
+smoothing_coeff = 1e-6
 
 
 def iou(preds: torch.Tensor,
@@ -10,7 +12,7 @@ def iou(preds: torch.Tensor,
 
     :param preds: batch of mask predictions
     :param labels: batch of mask labels
-    :return: the IoU score for each pair
+    :return: the IoU score for each pair in the batch
     """
 
     # If the outputs are from a neural network they might have 4 dimensions (batch_size, 1, width, height)
@@ -20,7 +22,23 @@ def iou(preds: torch.Tensor,
     intersections = (preds & labels).long().sum((1, 2))
     unions = (preds | labels).long().sum((1, 2))
 
-    return (intersections + IoU_smoothing_coeff) / (unions + IoU_smoothing_coeff)
+    return (intersections + smoothing_coeff) / (unions + smoothing_coeff)
+
+
+def dice_loss(preds: torch.Tensor,
+              labels: torch.Tensor):
+    """
+    Compute the dice loss given batches of predictions and labels
+
+    :param preds: batch of mask predictions
+    :param labels: batch of mask labels
+    :return: the dice loss for each pair in the batch
+    """
+    # If the outputs are from a neural network they might have 4 dimensions (batch_size, 1, width, height)
+    if preds.ndim == 4:
+        preds = preds.squeeze(1)
+
+    return (2. * preds.eq(labels).long().sum() + smoothing_coeff) / (labels.sum() + preds.sum() + smoothing_coeff)
 
 
 def pixel_accuracy(preds: torch.Tensor,
@@ -30,10 +48,10 @@ def pixel_accuracy(preds: torch.Tensor,
 
     :param preds: batch of mask predictions
     :param labels: batch of mask labels
-    :return: the pixel accuracy score
+    :return: the pixel accuracy score for each pair in the batch
     """
     # If the outputs are from a neural network they might have 4 dimensions (batch_size, 1, width, height)
     if preds.ndim == 4:
         preds = preds.squeeze(1)
 
-    return preds.eq(labels).long().sum() / labels.numel()
+    return (preds.eq(labels).long().sum() + smoothing_coeff) / (labels.numel() + smoothing_coeff)
