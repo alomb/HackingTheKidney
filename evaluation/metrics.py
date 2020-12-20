d99331bf505d55524aa601dc50341c9bbfad902c
alessandro <alelombo55@gmail.com>
2020-12-20 01:02:21 +0100

    Dice and Focal loss functions. Fix metrics.

--- evaluation/metrics.py
+++ evaluation/metrics.py
@@ -11,8 +11,8 @@ def iou(preds: Tensor,
     Compute IoU scores given batches of predictions and labels
     The IoU is referred also as the Jaccard index.
 
-    :param preds: batch of mask predictions
-    :param labels: batch of mask labels
+    :param preds: batch of binary mask predictions
+    :param labels: batch of binary mask labels
     :return: the IoU score for each pair in the batch
     """
     # If the outputs are from a neural network they might have 4 dimensions (batch_size, 1, width, height)
@@ -33,15 +33,16 @@ def dice_coefficient(preds: Tensor,
     https://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient.
     This is the metric used in the competition.
 
-    :param preds: batch of mask predictions
-    :param labels: batch of mask labels
+    :param preds: batch of binary mask predictions
+    :param labels: batch of binary mask labels
     :return: the dice coefficient for each pair in the batch
     """
     # If the outputs are from a neural network they might have 4 dimensions (batch_size, 1, width, height)
     if preds.ndim == 4:
         preds = preds.squeeze(1)
 
-    return (2. * preds.eq(labels).long().sum() + smoothing_coeff) / (labels.sum() + preds.sum() + smoothing_coeff)
+    return (2. * preds.eq(labels).long().sum((1, 2)) + smoothing_coeff) /\
+           (labels.sum((1, 2)) + preds.sum((1, 2)) + smoothing_coeff)
 
 
 def pixel_accuracy(preds: Tensor,
@@ -50,12 +51,13 @@ def pixel_accuracy(preds: Tensor,
 
     Compute the pixel accuracy score given batches of predictions and labels.
 
-    :param preds: batch of mask predictions
-    :param labels: batch of mask labels
+    :param preds: batch of binary mask predictions
+    :param labels: batch of binary mask labels
     :return: the pixel accuracy score for each pair in the batch
     """
     # If the outputs are from a neural network they might have 4 dimensions (batch_size, 1, width, height)
     if preds.ndim == 4:
         preds = preds.squeeze(1)
 
-    return (preds.eq(labels).long().sum() + smoothing_coeff) / (labels.numel() + smoothing_coeff)
+    return (preds.eq(labels).long().sum((1, 2)) + smoothing_coeff) /\
+           (labels.shape[1] * labels.shape[2] + smoothing_coeff)
---
+++ training/loss_functions.py
@@ -0,0 +1,71 @@
+import torch
+import torch.nn.functional as F
+from torch import Tensor
+from torch.nn import Module
+
+from evaluation.metrics import dice_coefficient
+
+
+class BinaryDiceLoss(Module):
+    """
+
+    Computes the dice loss of each pair of masks in a batch and then average them.
+    Masks are expected to be binary (1 and 0).
+
+    dice_loss = (1 - dice_coefficient)
+    """
+    def __init__(self):
+        super(BinaryDiceLoss, self).__init__()
+
+    def forward(self,
+                preds: Tensor,
+                labels: Tensor) -> Tensor:
+        """
+
+        :param preds: predicted binary masks
+        :param labels: ground truth binary masks
+        :return: the mean of (1 - dice_coefficient) for each pair of masks in the batch
+        """
+
+        return torch.mean(1. - dice_coefficient(preds, labels))
+
+
+class BinaryFocalLoss(Module):
+    """
+
+    Computes the focal loss of each pair of masks in the batch and then average them.
+    Masks are expected to be binary (1 and 0).
+
+    focal_loss = alpha * (1 - pt)^(gamma) * log(pt)
+    https://arxiv.org/abs/1708.02002
+    """
+    def __init__(self,
+                 gamma: float = 0.0,
+                 alpha: float = 1.0):
+        """
+
+        :param gamma: the focusing parameter (e.g. 0, 0.5, 1, 2, 5)
+        :param alpha: the weight to apply to the foreground class
+        """
+        super(BinaryFocalLoss, self).__init__()
+        self.gamma = gamma
+        self.alpha = alpha
+
+    def forward(self,
+                preds: Tensor,
+                labels: Tensor) -> Tensor:
+        """
+
+        :param preds: predicted binary masks
+        :param labels: ground truth binary masks
+        :return: the mean of the focal loss of each pair of masks in the batch
+        """
+        if preds.ndim == 4:
+            preds = preds.squeeze(1)
+
+        logpt = F.binary_cross_entropy(preds,
+                                       labels,
+                                       reduction='none')
+
+        pt = torch.clamp(-logpt.exp(), min=-100)
+        return (self.alpha * ((1 - pt) ** self.gamma) * logpt).mean()
